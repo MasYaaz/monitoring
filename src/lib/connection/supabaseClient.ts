a5bca6f2e22a9442d5e5bcc7b592980cb987af73
Aflah Mahdi Yazdi <Diyaz.hal22@gmail.com>
2025-08-20 17:47:59 +0700

    new

--- src/lib/connection/supabaseClient.ts
+++ src/lib/connection/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Ambil dari Supabase Project Settings → API
-const supabaseUrl = "https://gpjwdlxsodyiwmvljpmg.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwandkbHhzb2R5aXdtdmxqcG1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU0NDA0MzcsImV4cCI6MjA3MTAxNjQzN30.zN15C8NHVAOgvAaU_WwpKNNC3-qwjQq1dywW56mReOI";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
